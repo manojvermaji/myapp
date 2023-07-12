14129cb96f1166121d2473cc00b4ff4c093eed25
SanskarEng <sanskar.sahu@preciousinfosystem.com>
2023-07-12 18:28:43 +0530

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :posts
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+ 
+  root "posts#index"
+
+  # root "articles#index"
+end
